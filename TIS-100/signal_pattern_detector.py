eb684140bda2a76fd4bfb9fd3284dcc4ef968927
Dez <malisgotit@yahoo.com>
2023-09-25 13:57:46 +1000

    Add main function & fix docstrings

--- TIS-100/signal_pattern_detector.py
+++ TIS-100/signal_pattern_detector.py
@@ -1,3 +1,14 @@
 """
-Replicate TIS-100 'Signal Patter Detector' problem & solution
+Program for replicating TIS-100 'Signal Patter Detector' problem & solution
 """
+
+
+from random import randint
+
+
+def main():
+    """Replicate Signal Pattern Detector problem & solution"""
+    print()
+
+
+main()
