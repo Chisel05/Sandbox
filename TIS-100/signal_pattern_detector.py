ecf138b3d24ac209fbf2f69d5aeb887726324b73
Dez <malisgotit@yahoo.com>
2023-09-25 14:32:27 +1000

    Add Signal Patter Detector algorithm, without function refactoring

--- TIS-100/signal_pattern_detector.py
+++ TIS-100/signal_pattern_detector.py
@@ -2,13 +2,25 @@
 Program for replicating TIS-100 'Signal Patter Detector' problem & solution
 """
 
-
 from random import randint
 
 
 def main():
     """Replicate Signal Pattern Detector problem & solution"""
-    print()
+    zeros = []
+    number = randint(0, 4)
+    while len(zeros) < 4:
+        if number == 0:
+            while number == 0 or len(zeros) != 4:
+                zeros.append(0)
+                print(number)
+                number = randint(0, 4)
+        else:
+            zeros = []
+            number = randint(0, 4)
+            print(number)
+    print(f"WOW! You got FOUR zeros in a row?\n"
+          f"List: {zeros}")
 
 
 main()
